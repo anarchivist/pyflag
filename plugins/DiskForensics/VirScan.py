c1e670d0ca2556c6bde9527a334c6befcd855028
scudette <scudette@users.sf.net>
2005-02-11 18:29:36 -0500

    Added Copyright statements for all files that dont have them
    darcs-hash:20050211232936-f1522-532395616bbb538780f2a0c730af2be515f71ac7.gz

--- plugins/DiskForensics/VirScan.py
+++ plugins/DiskForensics/VirScan.py
@@ -1,3 +1,24 @@
+# Michael Cohen <scudette@users.sourceforge.net>
+# David Collett <daveco@users.sourceforge.net>
+#
+# ******************************************************
+#  Version: FLAG $Name:  $ $Date: 2004/10/26 01:07:53 $
+# ******************************************************
+#
+# * This program is free software; you can redistribute it and/or
+# * modify it under the terms of the GNU General Public License
+# * as published by the Free Software Foundation; either version 2
+# * of the License, or (at your option) any later version.
+# *
+# * This program is distributed in the hope that it will be useful,
+# * but WITHOUT ANY WARRANTY; without even the implied warranty of
+# * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+# * GNU General Public License for more details.
+# *
+# * You should have received a copy of the GNU General Public License
+# * along with this program; if not, write to the Free Software
+# * Foundation, Inc., 59 Temple Place - Suite 330, Boston, MA  02111-1307, USA.
+# ******************************************************
 """ This modules add support for the ClamAV virus scanner to virus scan all files in the filesystem.
 
 We provide a scanner class and a report to query the results of this scanner.
--- plugins/Farm.py
+++ plugins/Farm.py
@@ -1,8 +1,15 @@
+# ******************************************************
+# Copyright 2004: Commonwealth of Australia.
+#
+# Developed by the Computer Network Vulnerability Team,
+# Information Security Group.
+# Department of Defence.
+#
 # Michael Cohen <scudette@users.sourceforge.net>
 # David Collett <daveco@users.sourceforge.net>
 #
 # ******************************************************
-#  Version: FLAG $Name:  $ $Date: 2004/10/26 01:07:53 $
+#  Version: FLAG  $Name:  $ $Date: 2004/10/23 15:48:12 $
 # ******************************************************
 #
 # * This program is free software; you can redistribute it and/or
@@ -19,36 +26,8 @@
 # * along with this program; if not, write to the Free Software
 # * Foundation, Inc., 59 Temple Place - Suite 330, Boston, MA  02111-1307, USA.
 # ******************************************************
-""" Implement a simple logger wrapper.
-
-logging verbosity is controlled through the configuration variable LOG_LEVEL
-"""
+""" This module implements reports enabling the remote farming of scanners """
 import pyflag.conf
 config=pyflag.conf.ConfObject()
-import sys,traceback
-
-## These are predefined logging levels:
-ERRORS=1
-WARNINGS=2
-INFO=3
-DEBUG=7
-RING_BUFFER_SIZE=20
-
-ring_buffer = []
-
-lookup = { ERRORS: "Critical Error", WARNINGS: "Warning", INFO:"Infomation",DEBUG:"Debug"}
-
-def log(level,message):
-    """ Prints the message out only if the configured verbosity is higher than the message's level."""
-    global ring_buffer
-    string = "%s: %s" % (lookup[level],message)
-    
-    if config.LOG_LEVEL>=level:
-        print string
-        ring_buffer.append(string)
-
-        if len(ring_buffer)>RING_BUFFER_SIZE:
-            ring_buffer=ring_buffer[-RING_BUFFER_SIZE:]
-        
-    if level<=ERRORS:
-        print traceback.print_tb(sys.exc_info()[2])
+import pyflag.logging as logging
+import pyflag.Scanner as Scanner
