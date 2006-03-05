bd81d8123092494e2261ebfd74d1b8acfbf22e97
michael.cohen <michael.cohen@netspeed.com.au>
2006-03-05 00:02:49 -0500

    Changed libevf license to BSD to help incorporate into AFFlib.
    darcs-hash:20060305050249-0996e-d5b618693fa8e0500baf3373e89348d47fbc3ffd.gz

--- src/lib/libevf.h
+++ src/lib/libevf.h
@@ -4,20 +4,28 @@
 # ******************************************************
 #  Version: FLAG $Version: 0.80.1 Date: Tue Jan 24 13:51:25 NZDT 2006$
 # ******************************************************
-#
-# * This program is free software; you can redistribute it and/or
-# * modify it under the terms of the GNU General Public License
-# * as published by the Free Software Foundation; either version 2
-# * of the License, or (at your option) any later version.
-# *
-# * This program is distributed in the hope that it will be useful,
-# * but WITHOUT ANY WARRANTY; without even the implied warranty of
-# * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
-# * GNU General Public License for more details.
-# *
-# * You should have received a copy of the GNU General Public License
-# * along with this program; if not, write to the Free Software
-# * Foundation, Inc., 59 Temple Place - Suite 330, Boston, MA  02111-1307, USA.
+
+This library was relicensed on the 5th March 2006 to the modified BSD
+licence.  This allows this library to be used in commercial
+applications, and other OSS projects using the BSD license. This move
+was done in order to try and create a freely available codebase for
+applications that need to be able to read encase file format. The
+proprietary Encase file format is currently an impediment to forensics
+application compatibility and openness in judicial
+processes. Hopefully the release of this library can serve to help in
+removing this impediment. Note that the rest of PyFlag is still
+released under the GPL, the BSD license only applies to this library.
+
+Modified BSD License:
+
+Redistribution and use in source and binary forms, with or without modification, are permitted provided that the following conditions are met:
+
+   1. Redistributions of source code must retain the above copyright notice, this list of conditions and the following disclaimer.
+   2. Redistributions in binary form must reproduce the above copyright notice, this list of conditions and the following disclaimer in the documentation and/or other materials provided with the distribution.
+   3. The name of the author may not be used to endorse or promote products derived from this software without specific prior written permission.
+
+THIS SOFTWARE IS PROVIDED BY THE AUTHOR ``AS IS'' AND ANY EXPRESS OR IMPLIED WARRANTIES, INCLUDING, BUT NOT LIMITED TO, THE IMPLIED WARRANTIES OF MERCHANTABILITY AND FITNESS FOR A PARTICULAR PURPOSE ARE DISCLAIMED. IN NO EVENT SHALL THE AUTHOR BE LIABLE FOR ANY DIRECT, INDIRECT, INCIDENTAL, SPECIAL, EXEMPLARY, OR CONSEQUENTIAL DAMAGES (INCLUDING, BUT NOT LIMITED TO, PROCUREMENT OF SUBSTITUTE GOODS OR SERVICES; LOSS OF USE, DATA, OR PROFITS; OR BUSINESS INTERRUPTION) HOWEVER CAUSED AND ON ANY THEORY OF LIABILITY, WHETHER IN CONTRACT, STRICT LIABILITY, OR TORT (INCLUDING NEGLIGENCE OR OTHERWISE) ARISING IN ANY WAY OUT OF THE USE OF THIS SOFTWARE, EVEN IF ADVISED OF THE POSSIBILITY OF SUCH DAMAGE.
+
 # ******************************************************
 */
 #include <stdio.h>
