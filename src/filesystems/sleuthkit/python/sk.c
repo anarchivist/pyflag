a6c38279f2f0acb5b2533c55a7b14522ec07c627
davico <davico@tpg.com.au>
2006-11-21 06:25:58 -0500

    more work in python module
    darcs-hash:20061121112558-84eb5-090b170042086317bde4f35021b1f1fa5f0a41be.gz

--- src/filesystems/sleuthkit/python/sk.c
+++ src/filesystems/sleuthkit/python/sk.c
@@ -45,16 +45,17 @@ getblocks_walk_callback (FS_INFO *fs, DADDR_T addr, char *buf, int size, int fla
 
     /* create a new block entry */
     b = talloc(blocks, struct block);
-    b->resdata = NULL;
 
     if(flags & FS_FLAG_DATA_RES) {
         // we have resident ntfs data (yuck!)
-        b->resdata = (char *)talloc_size(b, size);
+        b->addr = 0;
         b->size = size;
+        b->resdata = (char *)talloc_size(b, size);
         memcpy(b->resdata, buf, size);
     } else {
         b->addr = addr;
         b->size = size;
+        b->resdata = NULL;
     }
 
     /* add to the list */
@@ -62,6 +63,7 @@ getblocks_walk_callback (FS_INFO *fs, DADDR_T addr, char *buf, int size, int fla
     return WALK_CONT;
 }
 
+/* lookup an inode from a path */
 INUM_T lookup(FS_INFO *fs, char *path) {
     return fs_ifind_path_ret(fs, 0, path);
 }
@@ -202,15 +204,16 @@ static PyTypeObject skfsType = {
 typedef struct {
     PyObject_HEAD
     PyObject *skfs;
-    struct block *blocks;
-    off_t readptr;
     FS_INODE *fs_inode;
+    struct block *blocks;
+    long long readptr;
 } skfile;
 
 static void
 skfile_dealloc(skfile *self) {
     Py_XDECREF(self->skfs);
     talloc_free(self->blocks);
+    fs_inode_free(self->fs_inode);
     self->ob_type->tp_free((PyObject*)self);
 }
 
@@ -218,47 +221,99 @@ static int
 skfile_init(skfile *self, PyObject *args, PyObject *kwds) {
     char *filename=NULL;
     INUM_T inode=0;
-    PyObject *skfs;
+    PyObject *skfs_obj;
+    FS_INFO *fs;
 
     static char *kwlist[] = {"filesystem", "inode", "filename", NULL};
 
     if(!PyArg_ParseTupleAndKeywords(args, kwds, "O|Ks", kwlist, 
-                                    &skfs, &inode, &filename))
+                                    &skfs_obj, &inode, &filename))
         return -1; 
 
-//    if(filename)
-//        inode = lookup(filename);
+    fs = ((skfs *)skfs_obj)->fs;
+
+    if(filename)
+        inode = lookup(fs, filename);
 
     /* must specify either inode or filename */
     if(filename==NULL && inode <= 0)
         return -1;
 
     /* can we lookup this inode? */
-//    self->fs_inode = self->fs->inode_lookup(self->fs, inode);
-//    if(self->fs_inode == NULL)
-//        return -1;
+    self->fs_inode = fs->inode_lookup(fs, inode);
+    if(self->fs_inode == NULL)
+        return -1;
 
     /* store a ref to the skfs */
-    Py_INCREF(skfs);
-    self->skfs = skfs;
+    Py_INCREF(skfs_obj);
+    self->skfs = skfs_obj;
 
     /* perform a file run and populate the block list */
     self->blocks = talloc(NULL, struct block);
     INIT_LIST_HEAD(&self->blocks->list);
-//    self->fs->file_walk(self->skfs->fs, self->fs_inode, 0, 0, 
-//                       (FS_FLAG_FILE_AONLY | FS_FLAG_FILE_RECOVER | FS_FLAG_FILE_NOSPARSE),
-//                       (FS_FILE_WALK_FN) getblocks_walk_callback, (void *)self->blocks);
+    fs->file_walk(fs, self->fs_inode, 0, 0, 
+                 (FS_FLAG_FILE_AONLY | FS_FLAG_FILE_RECOVER | FS_FLAG_FILE_NOSPARSE),
+                 (FS_FILE_WALK_FN) getblocks_walk_callback, (void *)self->blocks);
 
+    self->readptr = 0;
     return 0;
 }
 
 /* read data from a file */
 static PyObject *
 skfile_read(skfile *self, PyObject *args) {
-    long int size;
+    char *buf;
+    int cur, written;
+    PyObject *retdata;
+    FS_INFO *fs;
+    struct block *b;
+    int readlen=-1;
 
-    if(!PyArg_ParseTuple(args, "l", &size))
+    fs = ((skfs *)self->skfs)->fs;
+
+    if(!PyArg_ParseTuple(args, "|i", &readlen))
         return NULL; 
+
+    /* adjust readlen if size not given or is too big */
+    if(readlen < 0 || self->readptr + readlen > self->fs_inode->size)
+        readlen = self->fs_inode->size - self->readptr;
+    
+    /* allocate buf, be generous in case data straddles blocks */
+    buf = (char *)malloc(readlen + (2 * fs->block_size));
+    if(!buf)
+        return NULL;
+
+    /* read necessary blocks into buf */
+    cur = written = 0;
+    list_for_each_entry(b, &self->blocks->list, list) {
+
+        /* deal with NTFS resident files */
+        if(b->resdata) {
+            memcpy(buf, b->resdata, b->size);
+            break;
+        }
+        
+        /* we don't need any data in this block, skip */
+        if(cur + fs->block_size <= self->readptr) {
+            cur += fs->block_size;
+            continue;
+        }
+
+        /* read block into buf */
+        fs_read_block_nobuf(fs, buf+written, fs->block_size, b->addr);
+        cur += fs->block_size;
+        written += fs->block_size;
+
+        /* are we done yet? */
+        if(cur >= self->readptr + readlen)
+            break;
+    }
+
+    /* copy what we want into the return string */
+    retdata = PyString_FromStringAndSize(buf + (self->readptr % fs->block_size), readlen);
+    free(buf);
+
+    return retdata;
 }
 
 static PyMethodDef skfile_methods[] = {
