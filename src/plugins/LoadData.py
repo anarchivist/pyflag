65969067bd3d7c76fc7f84466a4e3ef10985cd79
alan.leigh <alan.leigh@gmail.com>
2007-04-12 08:00:16 -0400

    typo for progress
    darcs-hash:20070412120016-1ab04-93e085568d072404efcc3e5c79a321d840b02918.gz

--- src/plugins/LoadData.py
+++ src/plugins/LoadData.py
@@ -382,7 +382,7 @@ class ScanFS(Reports.report):
         size = dbh.fetch()['size']
 
         pagesize=20
-        dbh.execute("select timestamp,level,message from logs limit %s, %s", (min(size-pagesize,0), pagesize))
+        dbh.execute("select timestamp,level,message from logs limit %s, %s", (max(size-pagesize,0), pagesize))
         data = '\n'.join(["%(timestamp)s(%(level)s): %(message)s" % row for row in dbh])
         tmp=result.__class__(result)
         tmp.text(data,font='typewriter',style="red")
