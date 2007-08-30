18983c47b03bca0c6a1668820480f58e4189f2f8
alan.leigh <alan.leigh@gmail.com>
2007-08-29 22:24:30 -0400

    Tiny changes to flash logging
    darcs-hash:20070830022430-1ab04-f3f4bc963ac46da95054e1138ee7bf1719a96dd8.gz

--- src/plugins/Flash/BasicCommands.py
+++ src/plugins/Flash/BasicCommands.py
@@ -462,7 +462,7 @@ class execute(pyflagsh.command):
         start_time=time.time()
         report,query = self.prepare()
 
-        pyflaglog.log(pyflaglog.VERBOSE_DEBUG, "Will execute the following query %s" % query)
+        pyflaglog.log(pyflaglog.DEBUG, "Flash will execute the following query: %s" % query)
 
         ## Instantiate the report
         report=report(self.environment._flag)
@@ -490,10 +490,12 @@ class execute(pyflagsh.command):
             report.display(query,result)
             yield result.display()
             yield "Execution of %s successful in %s sec" % (self.args[1],time.time()-start_time)
+            pyflaglog.log(pyflaglog.DEBUG, "Flash successfully ran the following query: %s" % query)
         except Exception,e:
             import traceback
             print traceback.print_tb(sys.exc_info()[2])
             raise RuntimeError("%s: %s after %s sec" %  (sys.exc_info()[0],sys.exc_info()[1],time.time()-start_time))
+            pyflaglog.log(pyflaglog.WARNING, "Flash failed to run the following query: %s" % query)
 
 class reset(execute):
     """ Resets the given report """
