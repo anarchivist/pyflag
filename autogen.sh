0d9bcff623b2e8145cefd0f3e7dcfa3775d098bf
Phil.H <Phil.H@gmx.net>
2008-05-20 03:16:13 -0400

    autogen.sh improvements
    darcs-hash:20080520071613-43390-dd076a1ded4dff5ed86aa0bc80744bade04b512f.gz

--- autogen.sh
+++ autogen.sh
@@ -1,10 +1,71 @@
 #! /bin/sh
 
-libtoolize
-aclocal -I config
-autoheader
-automake --add-missing --copy
-autoconf
+echo PyFlag autogen.sh
+echo
+
+res=`type libtoolize`
+if [ $0 = 0 ]; then
+	echo please install libtoolize
+	exit 1
+else
+	echo starting libtoolize
+	libtoolize
+	echo finished libtoolize
+fi
+
+echo
+
+res=`type aclocal`
+if [ $0 = 0 ]; then
+	echo please install aclocal
+	exit 1
+else
+	echo starting aclocal
+	aclocal -I config
+	echo finished aclocal
+fi
+
+echo
+
+res=`type autoheader`
+if [ $0 = 0 ]; then
+    echo please install autoheader
+    exit 1
+else
+    echo starting autoheader
+	autoheader
+    echo finished autoheader
+fi
+
+echo
+
+res=`type automake`
+if [ $0 = 0 ]; then
+    echo please install automake
+    exit 1
+else
+    echo starting automake
+	automake --add-missing --copy
+	echo finished automake
+fi
+
+echo
+
+res=`type autoconf`
+if [ $0 = 0 ]; then
+    echo please install autoconf
+    exit 1
+else
+    echo starting autoconf
+	autoconf
+	echo finished autoconf
+fi
+
+echo
 
 ## Fix up permissions of some files:
 chmod +x tests/pyflag tests/pyflash debian/rules
+
+echo everything done...
+echo
+echo next steps are running configure, make, make install
