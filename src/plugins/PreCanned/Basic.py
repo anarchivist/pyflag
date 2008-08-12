57ac475903f0201eff4a1cac829628abf06d37ce
scudette <scudette@users.sf.net>
2008-08-12 15:51:17 -0400

    Added a forgotten file.
    darcs-hash:20080812195117-f1522-273f3ccf7734dc951812b03deeb285d83c9cc335.gz

---
+++ src/plugins/PreCanned/Basic.py
@@ -0,0 +1,32 @@
+""" These are PreCanned Reports.
+
+PreCanned Reports are the PyFlag equivalent of the google 'Im Feeling
+Lucky' feature - we basically just dump out some simple queries which
+are used to get you started.
+"""
+
+import pyflag.Reports as Reports
+import pyflag.conf
+config=pyflag.conf.ConfObject()
+import pyflag.Registry as Registry
+
+class ImFeelingLucky(Reports.report):
+    """
+    'Im Feeling Lucky' is a report which does basic analysis to get
+    you started on the case. Select which kind of analysis you want to
+    do.
+    """
+    name = "Im Feeling Lucky"
+    family = "Case Management"
+
+    def display(self, query, result):
+        for cls in Registry.PRECANNED.classes:
+            cls().display(query, result)
+
+
+class Images(Registry.PreCanned):
+    args = {'filter':' "Thumbnail"  has_magic image and  "Size"  > 20000 ',
+            'order': 4, 'direction':1}
+    family = "Disk Forensics"
+    report = "Browse Types"
+    description = "View all images bigger than 20kb "
