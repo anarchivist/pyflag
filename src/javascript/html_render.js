3bc844a9f9cbe50ba38ea8ea3d9d3c7a5ecc72ef
scudette <scudette@users.sf.net>
2009-01-13 19:34:47 -0500

    Forgot html_render.js
    darcs-hash:20090114003447-f1522-bc7828277a27a2133d1d2291d049937a69d73165.gz

---
+++ src/javascript/html_render.js
@@ -0,0 +1,41 @@
+function getElementsByClass(searchClass,node,tag) {
+  var classElements = new Array();
+  if ( node == null )
+    node = document;
+  if ( tag == null )
+    tag = '*';
+  var els = node.getElementsByTagName(tag);
+  var elsLen = els.length;
+  var pattern = new RegExp("(^|\\s)"+searchClass+"(\\s|$)");
+  for (i = 0, j = 0; i < elsLen; i++) {
+    if ( pattern.test(els[i].className) ) {
+      classElements[j] = els[i];
+      j++;
+    }
+  }
+  return classElements;
+}
+
+function hide_links() {
+  var elements = getElementsByClass('overlay',null, 'div');
+
+  for(var i=0; i<elements.length; i++) {
+    elements[i].style.opacity = "20%";
+  };
+
+  var menu = document.getElementById("pf_link_menu");
+  menu.onclick = show_links;
+  menu.innerHTML = "Show links";
+};
+
+function show_links() {
+  var elements = getElementsByClass('overlay',null, 'div');
+
+  for(var i=0; i<elements.length; i++) {
+    elements[i].style.opacity = '1';
+  };
+
+  var menu = document.getElementById("pf_link_menu");
+  menu.onclick = hide_links;
+  menu.innerHTML = "Hide links";
+}
